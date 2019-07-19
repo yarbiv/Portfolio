cfde3175a1ffa5fe9b48b5e44a566b8bf8dc4caf
Yoav Arbiv <yoav.arbiv@gmail.com>
2019-07-19 14:43:41 -0400

    Add animation on link hover. Add resume.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import Link from './Link.js'
+import Link from './components/Link.js'
 
 function App() {
   return (
     <div class='center'>
       <h1>Yoav <span id='name-text'>Arbiv</span></h1>
-      <p className='above-line-text'>Aspiring developer.</p>
+      <p className='above-line-text'>Fullstack developer.</p>
       <p className='above-line-text'>Computer Engineering, class of 2022 at UWaterloo.</p>
       <hr id='line'></hr>
       <div class='link-parent'>
         <Link url="https://github.com/yarbiv">GITHUB</Link>
         <Link url="https://linkedin.com/in/yarbiv">LINKEDIN</Link>
-        <Link url="https://google.com">RESUME</Link>
+        <Link url={'/resume.pdf'} target="_blank">RESUME</Link>
         <Link url="https://google.com">ABOUT</Link>
         <Link url="mailto:yoav.arbiv@gmail.com">CONTACT</Link>
       </div>

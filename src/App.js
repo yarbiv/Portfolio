d4d1e607733fd2cc2a8caa26e7f6f67533354cbb
yarbiv <yoav.arbiv@gmail.com>
2019-07-20 21:58:58 -0400

    Switch to HashRouter for use with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import About from './components/About.js'
 import Home from './components/Home.js'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function NoMatch() {
   return (

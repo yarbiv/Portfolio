5061e49d067e164e8887d35aa3e5282396d75342
Yoav Arbiv <yoavarbiv@Mac.attlocal.net>
2025-10-05 20:47:03 -0700

    initial pages for scunt'

--- src/App.js
+++ src/App.js
@@ -6,8 +6,41 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function NoMatch() {
   return (
     <div>
-      <h1>Where do you think you're going?</h1>
+      <h1>Lost?</h1>
       <Link to="/" className='link-child'>Let me take you home :)</Link>
+    </div>
+  );
+}
+
+function HarbourCourtHint() {
+  return (
+    <div >
+      <h2>i <u>Harbour</u> feelings for you, but the ball's in your <u>Court</u></h2>
+      <h2>now remind me where we got picked up to taste a wonderful port?</h2>
+    </div>
+  );
+}
+
+function GiantsHint() {
+  return (
+    <div >
+      <img style={{width: '80vw'}} src='IMG_8188.jpeg' alt=''/>
+      <h2>Find an iron giant who went face to face with bats</h2>
+      <h2>Our next web address, pointed to by stats!</h2>
+      <h3>(separate with dashes!)</h3>
+    </div>
+  );
+}
+
+
+function ExploratoriumHint() {
+  return (
+    <div >
+      <img style={{width: '80vw'}} src='IMG_8074.jpeg' alt=''/>
+      <h1>It's been a little bit, let's open this lock!</h1>
+      <h2>How many hole-y banners are out front?</h2>
+      <h2>How many pumps can a firefighter use here?</h2>
+      <h2>What month was this picture taken?</h2>
     </div>
   );
 }
@@ -20,6 +53,9 @@ function App() {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/302-2992-10881-2062-3283-523-140-660-1903-1464-1526-387-557" component={HarbourCourtHint} />
+            <Route path="/stagecoachgreens" component={GiantsHint} />
+            <Route path="/explore" component={ExploratoriumHint} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
